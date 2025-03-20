752d8785c2f30fc418afad089ae4389815535d6d
Greyzian Pruna <greyzianpruna@gmail.com>
2025-03-20 13:28:55 +0800

    Create Quiz.java

---
+++ Quiz.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Banking Program
+
+        //Declare variables
+
+        String[] questions = {"What is the main function of a rounter?",
+                            "Which part of the computer is considered the brain?",
+                            "What year was Facebook launched?",
+                            "Who is known as the father of modern computers?",
+                            "What was the first programming language?"};
+
+        String[][] options = {{"1. Storing files", "2. Encrypting data", "3. Directing internet traffic", "4. Managing Passwords"},
+                            {"1. CPU", "2. Hard Drive", "3. RAM", "4. GPU"},
+                            {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
+                            {"1. Steve Jobs", "2. Bill Gates", "3. Allen Turing", "4. Charles Babbage"},
+                            {"1. COBOL", "2. C", "3. Fortran", "4. Assembly"}};
+
+        int[] answers = {3, 1, 2, 4, 3};
+        int score = 0;
+        int guess;
+
+        System.out.println("******************************");
+        System.out.println("Welcome to the Java Quiz Game!");
+        System.out.println("******************************");
+
+        for (int i = 0; i < questions.length; i++){
+            System.out.println(questions[i]);
+
+            for (String option : options[i]){
+                System.out.println(option);
+            }
+
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess == answers[i]) {
+                System.out.println("CORRECT");
+                score++;
+            }
+            else {
+                System.out.println("WRONG");
+            }
+        }
+
+        System.out.println("Your total score: "+ score);
+
+
+        scanner.close();
+    }
+}
